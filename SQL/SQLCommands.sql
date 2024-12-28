b99105343f4b72e376944194a85b9b1c358580ee
AbiRamalingam <theabirami.r@gmail.com>
2024-12-28 16:36:59 +0530

    Created SQL commands

---
+++ SQL/SQLCommands.sql
@@ -0,0 +1,10 @@
+DDL - Data Definition Language
+    create, alter, drop, truncate
+DML - Data Manipulation Language
+    insert, update, delete
+DCL - Data Control Language
+    Grant, Revoke
+DQL - Data Query Language
+    Select
+TCL - Transactional Control Language
+    commit, rollback, save point
